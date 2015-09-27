dc7badc1fda125ed074a538152aa6301addb02f1
Arseny Tsyro <arseny-tsyro@ya.ru>
2015-09-27 15:26:36 +0300

    Выделил класс-помощник для создания контактов

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,7 @@ __author__ = 'Arseniy'
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -10,89 +11,7 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-
-    def load_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def add_contact(self, contact):
-        wd = self.wd
-        # go to contact page
-        wd.find_element_by_link_text("add new").click()
-        # fill in names
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        # fill in company data
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        # fill in telephone numbers
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_num)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_num)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_num)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax_num)
-        # fill in emails
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        # fill in dates
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[17]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[17]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[16]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[16]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[12]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[12]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        # fill in additional info
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        # submit
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.load_home_page()
+        self.contact = ContactHelper(self)
 
     def load_login_page(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_contact(Contact(firstname="asdf", middlename="asdfasd", lastname="fdsfsd", nickname="sdfsd",
+    app.contact.create(Contact(firstname="asdf", middlename="asdfasd", lastname="fdsfsd", nickname="sdfsd",
                         title="sdfsdf", company="fdsfsdf", address="sdfdf", home_num="1212", mobile_num="12212",
                         work_num="21212", fax_num="50", email2="afasd@adf", email3="asdsd@asdf",
                         homepage="asdfsad.com", byear="1999", ayear="1980", address2="fdsafasdf", phone2="dsafdsf",
@@ -23,7 +23,7 @@ def test_add_contact(app):
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_contact(Contact(firstname="", middlename="", lastname="", nickname="",
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                         title="", company="", address="", home_num="", mobile_num="",
                         work_num="", fax_num="", email2="", email3="",
                         homepage="", byear="", ayear="", address2="", phone2="",
