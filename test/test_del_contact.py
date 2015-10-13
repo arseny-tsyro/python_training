24dce09222ae98a348476ef04c9e983df2046311
Arseny Tsyro <arseny-tsyro@ya.ru>
2015-10-13 19:26:35 +0300

    Реализовал проверки в удалении контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,6 +4,11 @@ from model.contact import Contact
 
 
 def test_del_first_contact(app):
+    old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Test"))
     app.contact.delete_first()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
