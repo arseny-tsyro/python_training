716feb77e3848f25778ec5b9c5617e3193a632de
Arseny Tsyro <arseny-tsyro@ya.ru>
2015-10-13 19:41:49 +0300

    Реализовал проверку списков в модификации контактов

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -4,7 +4,14 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="Test", lastname="Last")
+    contact.id = old_contacts[0].id
     if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="Test"))
-    app.contact.edit_first()
+        app.contact.create()
+    app.contact.edit_first(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
