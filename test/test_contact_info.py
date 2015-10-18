2d2728fc86a4102b17b167521ae0be52c6092abb
Arseny Tsyro <arseny-tsyro@ya.ru>
2015-10-18 17:04:59 +0400

    Объединил тесты телефонов и тест полей контактов в один файл

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -4,6 +4,21 @@ from random import randrange
 import re
 
 
+def test_phones_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
+def test_phones_on_contact_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.home_num == contact_from_edit_page.home_num
+    assert contact_from_view_page.mobile_num == contact_from_edit_page.mobile_num
+    assert contact_from_view_page.work_num == contact_from_edit_page.work_num
+    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+
+
 def test_info_from_some_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="John", lastname="Snow", address="Hollywood, 11", email2="john@ya.ru",
