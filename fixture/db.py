6bdb1e237eb868c4a8173d5244ce3217483da21d
Arseny Tsyro <arseny-tsyro@ya.ru>
2015-11-05 19:03:57 +0300

    Добавил тест равенства списка контактов из базы данных и веб интерфейса

--- fixture/db.py
+++ fixture/db.py
@@ -1,6 +1,7 @@
 __author__ = 'Arseniy'
 import mysql.connector
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -15,12 +16,24 @@ class DbFixture:
     def get_group_list(self):
         list = []
         cursor = self.connection.cursor()
-        # Проверить конструкцию with
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor.fetchall():
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, deprecated from addressbook")
+            for row in cursor.fetchall():
+                (id, firstname, lastname, deprecated) = row
+                if deprecated is None:
+                    list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
         finally:
             cursor.close()
         return list
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,8 +1,16 @@
 __author__ = 'Arseniy'
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
     db_list = map(lambda x: Group(id=x.id, name=x.name.strip()), db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    db_list = map(lambda x: Contact(id=x.id, firstname=x.firstname.strip(), lastname=x.lastname.strip()),
+                  db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
