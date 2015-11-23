d3a4e084799ae469f7e9e8a2f1b41fcac694eb66
Arseny Tsyro <arseny-tsyro@ya.ru>
2015-11-23 17:18:19 +0300

    Добавил BDD-тесты удаления и создания групп

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,13 @@
+__author__ = 'Arseniy'
+from pytest_bdd import scenario
+from .group_steps import *
+
+
+@scenario('groups.feature', 'Add new group')
+def test_add_new_group():
+    pass
+
+
+@scenario('groups.feature', 'Delete a group')
+def test_del_group():
+    pass
